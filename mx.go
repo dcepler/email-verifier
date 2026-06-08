96d4ccf67e1bea21ce5ccb2e47f495ea06098b54
David Epler <dcepler@dcepler.net>
2026-06-08 13:29:03 -0400

    adding Enable/DisableMXCheck() option

--- mx.go
+++ mx.go
@@ -10,6 +10,12 @@ type Mx struct {
 
 // CheckMX will return the DNS MX records for the given domain name sorted by preference.
 func (v *Verifier) CheckMX(domain string) (*Mx, error) {
+	if !v.mxCheckEnabled {
+		return &Mx{
+			HasMXRecord: false,
+		}, nil
+	}
+
 	domain = domainToASCII(domain)
 	mx, err := net.LookupMX(domain)
 	if err != nil && len(mx) == 0 {
